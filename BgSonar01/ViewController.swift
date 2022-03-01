0e23daab7b0f72117f3e1c01a365bb323dd2c443
Natthanan Gumyan <Natthananebay@gmail.com>
2022-03-01 18:35:42 +0700

    add snapkit and create view

--- BgSonar01/ViewController.swift
+++ BgSonar01/ViewController.swift
@@ -6,11 +6,20 @@
 //
 
 import UIKit
-
+import SnapKit
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let newview = UIView()
+        newview.backgroundColor = .green
+        self.view.addSubview(newview)
+        newview.snp.makeConstraints { make in
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+            make.top.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
         // Do any additional setup after loading the view.
     }
 
